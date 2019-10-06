e627c8ac522bbea3dd1af0a8352263b2231acd3a
Truthnuggets <martincattell1618@outlook.com>
2019-10-06 21:36:50 +0100

    "Add tiles from Tile class"

--- Mijlieff.py
+++ Mijlieff.py
@@ -25,12 +25,36 @@ class MainWindow(object):
         self.p2_tile_frame = tkinter.Frame(self.root, height=50, bg="red")
         self.p2_tile_frame.grid(row=2, column=1)
         self.board_shape = 1
+        self.p1_tile_list = [Tile(1, 1, self.p1_tile_frame), Tile(1, 1, self.p1_tile_frame),
+                             Tile(2, 1, self.p1_tile_frame), Tile(2, 1, self.p1_tile_frame),
+                             Tile(3, 1, self.p1_tile_frame), Tile(3, 1, self.p1_tile_frame),
+                             Tile(4, 1, self.p1_tile_frame), Tile(4, 1, self.p1_tile_frame)]
+        self.p2_tile_list = [Tile(1, 2, self.p2_tile_frame), Tile(1, 2, self.p2_tile_frame),
+                             Tile(2, 2, self.p2_tile_frame), Tile(2, 2, self.p2_tile_frame),
+                             Tile(3, 2, self.p2_tile_frame), Tile(3, 2, self.p2_tile_frame),
+                             Tile(4, 2, self.p2_tile_frame), Tile(4, 2, self.p2_tile_frame)]
 
-
-        self.board_list =[]
+        self.board_list = []
         for i in range(4):
             b = BoardSection(self.game_frame)
             self.board_list.append(b)
+
+        self.p1_tile_list[0].label.grid(row=0, column=0)
+        self.p1_tile_list[1].label.grid(row=0, column=1)
+        self.p1_tile_list[2].label.grid(row=0, column=2)
+        self.p1_tile_list[3].label.grid(row=0, column=3)
+        self.p1_tile_list[4].label.grid(row=1, column=0)
+        self.p1_tile_list[5].label.grid(row=1, column=1)
+        self.p1_tile_list[6].label.grid(row=1, column=2)
+        self.p1_tile_list[7].label.grid(row=1, column=3)
+        self.p2_tile_list[0].label.grid(row=0, column=0)
+        self.p2_tile_list[1].label.grid(row=0, column=1)
+        self.p2_tile_list[2].label.grid(row=0, column=2)
+        self.p2_tile_list[3].label.grid(row=0, column=3)
+        self.p2_tile_list[4].label.grid(row=1, column=0)
+        self.p2_tile_list[5].label.grid(row=1, column=1)
+        self.p2_tile_list[6].label.grid(row=1, column=2)
+        self.p2_tile_list[7].label.grid(row=1, column=3)
 
     def board_builder(self):
         if self.board_shape == 1:
@@ -84,7 +108,10 @@ class MainWindow(object):
         global L_img
         global T_img
         global S_img
+        global options_window
         options_window = tkinter.Toplevel()
+        options_window.title("Options")
+        options_window.grab_set()
         select_label = tkinter.Label(options_window, text="Select a board shape").grid(row=0, column=0)
         button_frame = tkinter.Frame(options_window, bg="green", height=50)
         square_img = tkinter.PhotoImage(file="Square.gif")
@@ -114,7 +141,6 @@ class Game(object):
 
 
 class BoardSection(object):
-
     def __init__(self, frame):
         side = 110
         self.square_frame = tkinter.Frame(frame)
@@ -129,9 +155,42 @@ class BoardSection(object):
 
 
 class Tile(object):
-    def __init__(self, type):
-        self.type = type
-    pass
+    def __init__(self, typ, p, frame):
+        straightp1_img = tkinter.PhotoImage(file="straightp1.gif")
+        diagonalp1_img = tkinter.PhotoImage(file="diagonalp1.gif")
+        pullerp1_img = tkinter.PhotoImage(file="pullerp1.gif")
+        pusherp1_img = tkinter.PhotoImage(file="pusherp1.gif")
+        straightp2_img = tkinter.PhotoImage(file="straightp2.gif")
+        diagonalp2_img = tkinter.PhotoImage(file="diagonalp2.gif")
+        pullerp2_img = tkinter.PhotoImage(file="pullerp2.gif")
+        pusherp2_img = tkinter.PhotoImage(file="pusherp2.gif")
+        if p == 1:
+            if typ == 1:
+                self.label = tkinter.Label(frame, image=straightp1_img)
+                self.label.image = straightp1_img
+            elif typ == 2:
+                self.label = tkinter.Label(frame, image=diagonalp1_img)
+                self.label.image = diagonalp1_img
+            elif typ == 3:
+                self.label = tkinter.Label(frame, image=pullerp1_img)
+                self.label.image = pullerp1_img
+            elif typ == 4:
+                self.label = tkinter.Label(frame, image=pusherp1_img)
+                self.label.image = pusherp1_img
+        if p == 2:
+            if typ == 1:
+                self.label = tkinter.Label(frame, image=straightp2_img)
+                self.label.image = straightp2_img
+            elif typ == 2:
+                self.label = tkinter.Label(frame, image=diagonalp2_img)
+                self.label.image = diagonalp2_img
+            elif typ == 3:
+                self.label = tkinter.Label(frame, image=pullerp2_img)
+                self.label.image = pullerp2_img
+            elif typ == 4:
+                self.label = tkinter.Label(frame, image=pusherp2_img)
+                self.label.image = pusherp2_img
+
 
 
 main_window = MainWindow()
